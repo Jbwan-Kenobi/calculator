a82c6610a905f927a8bf26d15e54e873e94de53e
Jbwan-Kenobi <171935619+Jbwan-Kenobi@users.noreply.github.com>
2025-10-09 12:01:35 +0100

    Apply keyboard support

--- java.js
+++ java.js
@@ -39,6 +39,7 @@ const operate = function(firstNumber, secondNumber,operator) {
 const numbers = document.querySelectorAll(".number");
 const symbols = document.querySelectorAll(".symbol");
 var display = document.getElementById("display");
+const keyboard = document.querySelectorAll("buttons");
 
 numbers.forEach(number => {
 	number.addEventListener("click", () => {	
@@ -55,13 +56,14 @@ numbers.forEach(number => {
 				display.textContent = firstNumber + " " + firstOperator + " " + secondNumber;
 			};
 			//resets calculator when another number is pressed after equals
-			if(answer !== 0) {
+			/*if(answer !== 0) {
 				firstNumber = number.textContent;
 				secondNumber = "";
 				firstOperator = "";
 				answer = 0;
 				display.textContent = firstNumber;
-			};
+				console.log("1st O: " + firstOperator + " 1st N: " + firstNumber + " Ans: " + answer);
+			};*/
 
 		};
 
@@ -84,6 +86,12 @@ symbols.forEach(symbol => {
 			};
 			
 		} else {
+
+			/*if(firstOperator !== "" && firstOperator !== "=") {
+				display.textContent = display.textContent.slice(0, -3);
+				firstOperator = "";
+			};*/
+
 			//clear all from the calculator
 			if(symbol.textContent == "C" || firstNumber == "") {
 				firstNumber = "";
@@ -133,3 +141,90 @@ symbols.forEach(symbol => {
 
 	});
 });
+
+document.addEventListener('keydown', (event)=> {
+	if(/^-?\d+(\.\d+)?$/.test(event.key) == true) {
+	console.log(/^-?\d+(\.\d+)?$/.test(event.key));
+		if(firstNumber !== "" && firstOperator == "/" && number.textContent == "0") {
+				display.textContent = "Really!"
+		} else {
+
+			if(firstOperator == "") {
+				firstNumber += event.key;
+				display.textContent = firstNumber;
+			} else {
+				secondNumber += event.key;
+				display.textContent = firstNumber + " " + firstOperator + " " + secondNumber;
+			};
+		};
+	} else if (/[+\-*/=()^~%c]/.test(event.key) == true || event.key == "Enter") {
+		if(event.key =="Delete" || event.key == "Backspace") {
+			if(firstOperator == "" && secondNumber == "") {
+				firstNumber = firstNumber.slice(0, -1);
+				display.textContent = display.textContent.slice(0, -1);
+			} else if(firstNumber !== "" && firstOperator !== "" && secondNumber =="") {
+				firstOperator = ""
+				display.textContent = display.textContent.slice(0, -3);
+			} else {
+				secondNumber = secondNumber.slice(0, -1);
+				display.textContent = display.textContent.slice(0, -1);
+			};
+			
+		} else {
+
+			/*if(firstOperator !== "" && firstOperator !== "=") {
+				display.textContent = display.textContent.slice(0, -3);
+				firstOperator = "";
+			};*/
+
+			//clear all from the calculator
+			if(event.key == "c" || firstNumber == "") {
+				firstNumber = "";
+				secondNumber = "";
+				firstOperator = "";
+				secondOperator = "";
+				answer = 0;
+				display.textContent = "";
+			} else {
+
+				display.textContent += " " + event.key + " ";
+
+				if(event.key !== "=" && firstOperator == "") {
+					firstOperator = event.key;
+					}
+				else {
+					secondOperator = event.key;
+				};
+				//do the calculation
+				if(firstNumber !== "" && secondNumber !== "" && event.key !== "=") {
+					firstNumber = Number(firstNumber);
+					secondNumber = Number(secondNumber);
+					calc = firstOperator;
+					answer = Math.round(operate(firstNumber, secondNumber, calc)*10)/10;
+					display.textContent = answer + " " + event.key + " ";
+					firstNumber = answer;
+					secondNumber = "";
+					firstOperator = secondOperator;
+					secondOperator = ""
+				};
+				//if equals is pressed
+				if(event.key == "=" && secondNumber !==""|| event.key == "Enter" && secondNumber !== "") {
+					firstNumber = Number(firstNumber);
+					secondNumber = Number(secondNumber);
+					calc = firstOperator;
+					answer = Math.round(operate(firstNumber, secondNumber, calc)*10)/10;
+					display.textContent = answer;
+					firstNumber = answer;
+					secondNumber = "";
+					firstOperator = "";
+					secondOperator = "";
+				};
+
+			};
+
+		};
+
+	};
+	console.log(event.key);
+});
+    
